cdad7de2b8ea248c989ea6e461e1505ed40bef09
martin <venturamartin90@gmail.com>
2018-01-28 15:57:01 -0300

    first version

---
+++ Assets/Game/Splash/ChangeScene.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour {
+
+    public void ChangeSceneByName(string SCname = "Game")
+    {
+        SceneManager.LoadScene(SCname);
+    }
+}
